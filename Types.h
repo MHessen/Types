0a648d167110ae3aac43d4460598d79bd541d3b0
Mohammed Hessen <37942561+MHessen@users.noreply.github.com>
2018-12-25 01:42:22 +0200

    Add files via upload

---
+++ Types.h
@@ -0,0 +1,26 @@
+/*
+ * Types.h
+ *
+ * Author: Engr.Mohammed Hessen
+ */
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+
+/*
+	Header File Of Size Used Types
+*/
+
+typedef unsigned char u8 ;
+typedef unsigned int u16 ;
+typedef unsigned long int u32 ;
+typedef unsigned long long int u64 ;
+typedef signed char s8 ;
+typedef signed short int s16 ;
+typedef signed  int s32 ;
+typedef float f32 ;
+typedef double f64 ;
+
+
+#endif /* TYPES_H_ */
